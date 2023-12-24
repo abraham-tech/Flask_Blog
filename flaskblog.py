457829c3dc9c75af5bcab5c25e36c450ece12677
abraham <abraham.mathewos.meja@gmail.com>
2023-12-24 13:20:28 -0600

    Add: Login and Registration pages

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '78c4ce8924cc88d75128ce708249db51'
 
 posts = [
     {
@@ -32,6 +34,19 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
+
 
 
 if __name__ == '__main__':
