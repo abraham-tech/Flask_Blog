bc3c7de0ce2398e2cffa2ce93e51b9a24c936faa
abraham <abraham.mathewos.meja@gmail.com>
2023-12-24 12:51:52 -0600

    Add: static and templates files and update flask app

--- flaskblog.py
+++ flaskblog.py
@@ -1,19 +1,37 @@
-from flask import Flask;
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Alice',
+        'title': 'First Steps in Python',
+        'content': 'This is a blog post about getting started with Python.',
+        'date_posted': 'May 15, 2023'
+    },
+    {
+        'author': 'Bob',
+        'title': 'Understanding Decorators',
+        'content': 'This is a blog post about Python decorators.',
+        'date_posted': 'May 20, 2023'
+    },
+    {
+        'author': 'Charlie',
+        'title': 'Exploring Flask',
+        'content': 'This blog post explores the Flask web framework.',
+        'date_posted': 'May 23, 2023'
+    }
+]
 
 
 @app.route('/')
-def hello():
-    return "<h1>Hello World!</h1>"
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts)
 
 
 @app.route('/about')
 def about():
-    return "<h1>About Page</h1>"
-
-@app.route('/home')
-def home():
-    return "<h1>Home Page</h1>"
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
